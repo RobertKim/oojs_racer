590894469afa74a9328e05dcc2c2e7232dec3f72
Angie Bui <angiebui@gmail.com>
2013-05-01 08:01:23 -0700

    refactored

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  var p = '80';
-  var q = '81';
-
   $('form').on('submit', function(e){
     e.preventDefault();
     $.ajax({
@@ -9,55 +6,12 @@ $(document).ready(function() {
       method: "POST",
       data: $(this).serialize()
     }).done(function(server_data){ 
-      createGame(server_data);
+
+      game = createGame(server_data);
+      gameStart();
+
     }).fail(function(jqXHR, textStatus, errorThrown){
       $('body').append(jqXHR.responseText);
-    });
-  });
-
-  $('#start').on('click', function() {
-    $("#start").fadeOut();
-    game.startTime = new Date().getTime();
-
-    $(document).on('keyup', function(e){
-      var code = (e.keyCode);
-
-      // DRY - need to refactor to game.finish 
-      
-      if (code == p){
-        game.player1.advancePosition();
-        game.render("player1", game.player1);
-        if (game.player1.position == game.length){
-          alert(game.player1.name + " won!");
-          var d = new Date();
-          var endTime = (d.getTime() - game.startTime) / 1000
-          $.ajax({
-            url: '/game',
-            type: 'POST',
-            dataType: 'json',
-            data: {winner: game.player1.name, time: endTime}
-          }).always(function(){
-            $("#new_game").fadeIn();
-          })
-        }
-      } 
-      else if (code == q){
-        game.player2.advancePosition();
-        game.render("player2", game.player2);
-        if (game.player2.position == game.length){
-          alert(game.player2.name + " won!");
-          var d = new Date();
-          var endTime = (d.getTime() - game.startTime) / 1000
-          $.ajax({
-            url: '/game',
-            type: 'PUT',
-            dataType: 'json',
-            data: {winner: game.player2.name, time: endTime}
-          }).always(function(){
-            $("#new_game").fadeIn();
-          })
-        }
-      }
     });
   });
 });
--- public/js/objects.js
+++ public/js/objects.js
@@ -1,10 +1,27 @@
 function Player(name) {
   this.position = 0;
   this.name = name;
+};
+
+function createGame(server_data){
+  var p1, p2, length 
+  p1 = new Player(server_data.player1);
+  p2 = new Player(server_data.player2);
+  length = (server_data.length);
+  game = new Game(p1, p2, length)
+  $('.player-form').fadeOut(function(){
+    $('#track').fadeIn();
+  });
+  return game;
 }
 
 Player.prototype.advancePosition = function(){
   this.position ++; 
+};
+
+Game.prototype.render = function(player, obj) {
+  $("#" + player + "_strip").find('.active').removeClass('active');
+  $("#" + player + "_strip").find("#" + obj.position).addClass('active');
 }
 
 function Game(player1, player2, length) {
@@ -14,7 +31,55 @@ function Game(player1, player2, length) {
   this.winner = null;
 
   createBoard(player1, player2, length);
-}
+};
+
+Game.prototype.hasAWinner= function(){
+  if (this.player1.position == this.length){
+    this.winner = this.player1.name;
+  } else if (this.player2.position == this.length) {
+    this.winner = this.player2.name;
+  }
+};
+
+var gameStart = function(){
+  $('#start').on('click', function(){
+    $("#start").fadeOut();
+    game.startTime = new Date().getTime();
+  });
+  gameLogic();
+};
+
+var gameLogic = function() {
+  $(document).on('keyup', function(e){
+    if(game.winner === null){
+      if (e.keyCode == '80'){
+        game.player1.advancePosition();
+        game.render("player1", game.player1);
+        game.hasAWinner();
+      }else if (e.keyCode == '81'){
+        game.player2.advancePosition();
+        game.render("player2", game.player2);
+        game.hasAWinner();
+      }
+      if(game.winner != null){
+        var endTime = new Date().getTime() - game.startTime / 1000;
+        alert(game.winner + " won!");
+        var results = {winner: game.winner.name, time: endTime};
+        $.ajax({
+          url: '/game',
+          type: 'PUT',
+          dataType: 'json',
+          data: results
+        });
+        game.finish();
+      }
+    }
+  });
+};
+
+Game.prototype.finish = function() {
+  $("#new_game").fadeIn();
+};
 
 function createBoard(player1, player2, length) {
   this.length = length;
@@ -26,24 +91,4 @@ function createBoard(player1, player2, length) {
 
   $('#player1').html(player1.name);
   $('#player2').html(player2.name);
-}
-
-function createGame(server_data){
-  var p1, p2, length 
-  p1 = new Player(server_data.player1);
-  p2 = new Player(server_data.player2);
-  length = (server_data.length);
-  game = new Game(p1, p2, length)
-  $('.player-form').fadeOut(function(){
-    $('#track').fadeIn();
-  });
-}
-
-Game.prototype.render = function(player, obj) {
-  $("#" + player + "_strip").find('.active').removeClass('active');
-  $("#" + player + "_strip").find("#" + obj.position).addClass('active');
-}
-
-Game.prototype.finish = function(){
- // to refactor soon
 }
